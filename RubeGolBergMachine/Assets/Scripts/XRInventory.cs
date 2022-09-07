8be2ff433a62defc80eeba0f4e7be058bb97d0bc
Lucaswang8 <hi@madebylucas.co>
2022-09-06 22:19:51 -0700

    Added XRInventory script

--- RubeGolBergMachine/Assets/Scripts/XRInventory.cs
+++ RubeGolBergMachine/Assets/Scripts/XRInventory.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
+//Script purpose: attaching a gameobject to a certain anchor and having the ability to enable and disable it.
 public class XRInventory : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Inventory;
+    public GameObject Anchor;
+    private bool UIActive;
+    public InputActionReference buttonReference = null;
+
+    void Awake()
+    {
+        buttonReference.action.started += OpenInventory;
+    }
+    
+    private void OpenInventory(InputAction.CallbackContext obj)
+    {
+        UIActive = !UIActive;
+        Inventory.SetActive(UIActive);
+    }
+    
+
     void Start()
     {
-        
+        Inventory.SetActive(false);
+        UIActive = false;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (UIActive)
+        {
+            Inventory.transform.position = Anchor.transform.position;
+            Inventory.transform.eulerAngles =
+                new Vector3(Anchor.transform.eulerAngles.x + 15, Anchor.transform.eulerAngles.y, 0);
+        }
     }
 }
