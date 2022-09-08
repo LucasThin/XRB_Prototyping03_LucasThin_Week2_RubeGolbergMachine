6a61d345525c74902d678c63865de6304fde196a
Lucaswang8 <hi@madebylucas.co>
2022-09-07 21:10:36 -0700

    Added spawner

---
+++ RubeGolBergMachine/Assets/Scripts/SpawnButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SpawnButton : MonoBehaviour
+{
+    [SerializeField] private GameObject _toy;
+    [SerializeField] private Transform _spawnPosition;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other){
+        //_toy.SetActive(false);
+        Instantiate(_toy, _spawnPosition.position, quaternion.identity);
+        Debug.Log("Spawn");
+    }
+}
